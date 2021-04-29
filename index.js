b96603f4e45c1a026dd04078657637d782b3371a
Emmanuel <emmanuelamon92@gmail.com>
2021-04-29 12:41:17 -0400

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+// let wrapAdjective = (string = '*') => (special = 'special') => `You are ${special}`
+function wrapAdjective (string = '*') {
+    return function (special = 'special'){
+        return `You are ${string}${special}${string}!`
+    }
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
+// console.log(encouragingPromptFunction)
